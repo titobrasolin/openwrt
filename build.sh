fe5aeb813680a571e25da83f5ca317aaf3b4cc71
titobrasolin <tito.brasolin@gmai.com>
2016-04-17 10:39:23 +0200

    Now relying on customized xorg feed

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ echo "==========================================================================
 git clone --depth 1 -b $OPENWRT_GIT_BRANCH $OPENWRT_GIT_REPO openwrt
 cd openwrt
 cp feeds.conf.default feeds.conf
-echo "src-svn xorg svn://svn.mein.io/openwrt/feeds/xorg" >> feeds.conf
+echo "src-git xorg https://github.com/titobrasolin/openwrt-feeds-xorg.git" >> feeds.conf
 
 echo "============================================================================="
 echo "$(date -u) - Updating package feeds."
@@ -21,8 +21,6 @@ echo "==========================================================================
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-curl https://raw.githubusercontent.com/titobrasolin/openwrt/master/feeds_xorg_xorg_lib_libX11_disable_specs.patch | patch -d feeds -p0
-curl https://dev.openwrt.org/raw-attachment/ticket/19872/qt_not_available_from_nokia.patch | patch -d feeds -p0
 curl https://raw.githubusercontent.com/titobrasolin/openwrt/master/diffconfig > .config
 make defconfig
 # https://wiki.openwrt.org/doc/faq/development#building_on_multi-core_cpu
